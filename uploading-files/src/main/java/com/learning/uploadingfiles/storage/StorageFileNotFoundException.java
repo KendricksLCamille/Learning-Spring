9ea2478525b7619edb8d61a418eef615549887a3
kendricks <camilles20049@hotmail.com>
2024-05-31 16:29:35 -0400

    learned how to upload files in spring.

---
+++ uploading-files/src/main/java/com/learning/uploadingfiles/storage/StorageFileNotFoundException.java
@@ -0,0 +1,13 @@
+package com.learning.uploadingfiles.storage;
+
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ uploading-files/src/test/java/com/learning/uploadingfiles/UploadingFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.uploadingfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadingFilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
