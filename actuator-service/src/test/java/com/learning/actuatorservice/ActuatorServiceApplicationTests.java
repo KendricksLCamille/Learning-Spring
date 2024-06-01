c6871c0963266c4b8b39098d0d047de6c0f118c2
kendricks <camilles20049@hotmail.com>
2024-06-01 16:03:21 -0400

    learnt how to set up an acutuator with health checkck.

---
+++ actuator-service/src/test/java/com/learning/actuatorservice/ActuatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.actuatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
